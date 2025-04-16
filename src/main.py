6647290e66cf96c66b69588edb524a8f73d910e3
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-16 20:20:14 +0300

    Build PIpeline for response llm.

--- src/main.py
+++ src/main.py
@@ -8,13 +8,21 @@ try:
 
     from logs import log_debug, log_error, log_info
     from api import hello_routes, upload_route, to_chunks_route, llms_config_route
+    from utils import load_last_yaml
+    from llm import HuggingFcaeModel
 except Exception as e:
     msg = f"Import Error in: {os.path.dirname(__file__)}, Error: {e}"
     raise ImportError(msg)
 
+LLM_CONFIG = load_last_yaml( file_path=None)
+
+PROMPT = 
+model = 
 # Initialize FastAPI app
 app = FastAPI()
-
+generateror = HuggingFcaeModel()
+generateror.init_llm(**LLM_CONFIG)
+response = generateror.generate_response(prompt=prompt)
 # Include routers
 app.include_router(hello_routes, prefix="/api", tags=["Hello"])
 app.include_router(upload_route, prefix="/api", tags=["Upload File"])
