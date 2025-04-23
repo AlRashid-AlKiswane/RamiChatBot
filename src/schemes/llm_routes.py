f823935973e173de523e3857501b543e4ed6d863
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-23 14:39:55 +0300

    add new 2 paramters for quantization

--- src/schemes/llm_routes.py
+++ src/schemes/llm_routes.py
@@ -3,9 +3,11 @@ from pydantic import BaseModel
 
 class LLMResponse(BaseModel):
     model_name: str
-    temperature: Optional[float] = 0.7
-    max_new_tokens: Optional[int] = 128
-    top_p: Optional[float] = 0.95
-    top_k: Optional[int] = 50
-    do_sample: Optional[bool] = True
-    trust_remote_code: Optional[bool] = False
+    max_new_tokens: int 
+    temperature: float 
+    top_p: float
+    top_k: int 
+    trust_remote_code: bool 
+    do_sample: bool 
+    quantization: bool 
+    quantization_type: str 
