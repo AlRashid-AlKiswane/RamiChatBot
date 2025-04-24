4b15a8330a2a2c0190e5f2724f75c38b9650a6a2
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-24 14:40:35 +0300

    add to utiles extract_response

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,8 @@
 from .read_yaml import load_last_yaml
 from .extract_response import extract_assistant_response
+
+from llm import HuggingFcaeModel
+
+model_hugging = HuggingFcaeModel()
+
+model_hugging.init_llm()
