b11b32022fa066933c93a95ff4a51cfdc981dff2
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-24 13:51:35 +0300

    Add free line.

--- src/llm/huggingface_models.py
+++ src/llm/huggingface_models.py
@@ -126,4 +126,3 @@ class HuggingFcaeModel(LLMsInterface):
         except Exception as e:
             log_error(f"Error generating response. Prompt: {prompt[:60]}... Error: {e}")
             return "Error: Unable to generate response due to internal processing error."
-    
