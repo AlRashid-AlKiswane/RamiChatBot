ab1bc4ec2e873cfc6d2701fb9ca3e0be6997b33f
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-24 15:28:33 +0300

    remove not necessary import, for test i will write

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,2 @@
 from .read_yaml import load_last_yaml
 from .extract_response import extract_assistant_response
-
-from llm import HuggingFcaeModel
-
-model_hugging = HuggingFcaeModel()
-
-model_hugging.init_llm()
