b1704236bbbe1645fd955c228e3cd9fea6bf6831
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-24 20:10:19 +0300

    ignore all things, about dotenv, huggingFace, requests, urlib3, httpx, langchain.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,14 @@
 import logging
 
-# Suppress specific dotenv warnings
-logging.getLogger("dotenv").setLevel(logging.ERROR)
+# Suppress specific loggers
+suppress_loggers = [
+    "dotenv",
+    "HuggingFace",
+    "requests",
+    "urllib3",          
+    "httpx",            
+    "langchain",        
+]
+
+for logger_name in suppress_loggers:
+    logging.getLogger(logger_name).setLevel(logging.ERROR)
