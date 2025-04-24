7c0bde996ff76e8f45c948e9e5306129315ea75a
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-24 16:38:31 +0300

    add chat_mange_routes

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,4 +3,5 @@ from .upload_file import upload_route
 from .doc_to_chunks import to_chunks_route
 from .llms_config_routes import llms_config_route
 from .chat import generate_routes
+from .chat_histroy_manage import chat_mange_routes
 
