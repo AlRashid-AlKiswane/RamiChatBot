e586eb690efc8da6cc3fc4cca1d0a793ee971d32
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-24 18:04:43 +0300

    Change name dir from Database to dbs

---
+++ src/dbs/__init__.py
@@ -0,0 +1,3 @@
+from .create_sqlite_engin import create_sqlite_engine
+from .create_taples import create_chunks_table, create_embeddings_table, create_query_responses_table
+from .inset_to_database import insert_chunk, insert_embedding, insert_query_response
