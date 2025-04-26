8cfb246565b05d35e320397d49755852bfdaae27
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-26 17:26:33 +0300

    Add the new fucnation for pull to __init__ file.

--- src/dbs/__init__.py
+++ src/dbs/__init__.py
@@ -1,3 +1,4 @@
 from .create_sqlite_engin import create_sqlite_engine
 from .create_taples import create_chunks_table, create_embeddings_table, create_query_responses_table
 from .inset_to_database import insert_chunk, insert_embedding, insert_query_response
+from .pull_from_table import pull_from_table
