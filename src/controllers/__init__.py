ce9476cb8864b259b800d4a0779d5abb042c2437
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-28 21:31:31 +0300

    Remove convet chunks to embedding fucnation.

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,4 +1,3 @@
 from .ConvetDocsToChunks import load_and_chunk
 from .create_file_name import get_clean_file_name
 from .clear_taple_database import clear_table
-from .convert_text_to_embedding import convert_chunks_to_embedding
