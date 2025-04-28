7a9c335ee31c6593894b145fc952f6aa415dec88
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-04-28 21:32:14 +0300

    add embedding mdoel to __init__ file.

--- src/embedding/__init__.py
+++ src/embedding/__init__.py
@@ -1,4 +1,1 @@
-from .asset_model import  AssetModel 
-from .chunk_model import ChunkModel
-from .project_model import ProjectModel
-from .basedata_model import BaseDataModel
+from .sentence_model import EmbeddingModel
