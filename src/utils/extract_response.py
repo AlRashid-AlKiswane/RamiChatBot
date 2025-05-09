76b5cc3d1cbefc69079c25fc9033f3582f947edd
AlRashid AlKiswane <147489627+AlRashid-AlKiswane@users.noreply.github.com>
2025-05-09 01:52:42 +0000

    fix debug just extract the llm response wihtout titil

--- src/utils/extract_response.py
+++ src/utils/extract_response.py
@@ -4,7 +4,7 @@ def extract_assistant_response(text: str) -> str:
     """
     Extracts the response between <|ASSIST|> and <|END_ASSIST|> tags.
     """
-    texts = text.split("<|ASSIST|>")
+    texts = text.split("The answer:")
     if texts and len(texts) > 1:
         text = texts[1].split("<|END_ASSIST|>")[0].strip()  # Added strip() to clean up the response
     else:
