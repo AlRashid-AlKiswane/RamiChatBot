bd891a5a9416e2877dcd8fa5d6c2e41ec0639420
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-05-14 21:37:07 +0300

    Your code has been rated at 10.00/10 (previous run: 3.33/10, +6.67)

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,9 @@
+"""
+This script runs the FastAPI application using Uvicorn on host 0.0.0.0 and port 5000.
+It is designed to start the server with auto-reload during development.
+"""
+
 import uvicorn
 
 if __name__ == "__main__":
-	uvicorn.run("src.main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
