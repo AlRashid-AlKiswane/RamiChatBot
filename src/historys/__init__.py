aeea5d7b1f50e926401313201a3a5e78814ecc90
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-05-16 22:33:18 +0300

    make 10/10 pylint test

--- src/historys/__init__.py
+++ src/historys/__init__.py
@@ -1,1 +1,7 @@
-from .chathistroy import ChatHistoryManager
+"""
+historys package.
+
+Provides ChatHistoryManager for managing multi-user chat histories.
+"""
+
+from .chat_history_manager import ChatHistoryManager
