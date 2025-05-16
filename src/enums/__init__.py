fd1d97d3b249bf233e9dc900e06806381f01aaa4
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-05-16 22:22:22 +0300

    chore(enums): refactor all enums for full pylint compliance and style consistency

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -1,3 +1,8 @@
-from .HelloResponse import HelloResponse
-from .LLMsEnums import LLMsStatus
-from .YAMLFiles import YMLFileEnums
+"""
+This package contains Enum classes for standardizing system messages
+and configuration statuses used across the application.
+"""
+
+from .hello_response import HelloResponse
+from .llms_status import LLMsStatus
+from .yml_file_enums import YMLFileEnums
